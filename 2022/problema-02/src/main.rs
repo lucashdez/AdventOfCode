dd4549362cd14deb4f2ed1d277509c7be64646c5
lucashdez <lukastpiano@gmail.com>
2022-12-02 16:11:17 +0000

    P1

--- 2022/problema-02/src/main.rs
+++ 2022/problema-02/src/main.rs
@@ -8,8 +8,87 @@ fn read_file_filter(filename: String) -> Vec<String>
     result
 }
 
-fn p1() -> () {}
+fn points(rival: &str, mychoice: &str) -> i32
+{
+    let mut points = 0;
+    match mychoice
+    {
+        "X" =>
+        {
+            points += 1;
+            if rival == "A"
+            {
+                points += 3;
+            }
+            else if rival == "B"
+            {
+                points += 0;
+            }
+            else
+            {
+                points += 6;
+            }
+        }
+        "Y" =>
+        {
+            points += 2;
+            if rival == "A"
+            {
+                points += 6;
+            }
+            else if rival == "B"
+            {
+                points += 3;
+            }
+            else
+            {
+                points += 0;
+            }
+        }
+        "Z" =>
+        {
+            points += 3;
+            if rival == "A"
+            {
+                points += 0;
+            }
+            else if rival == "B"
+            {
+                points += 6;
+            }
+            else
+            {
+                points += 3;
+            }
+        }
+        _ =>
+        {
+            print!("wtf");
+            return 0;
+        }
+    }
+    return points;
+}
+
+fn p1(content: &Vec<String>) -> i32
+{
+    let mut final_points = 0;
+    for line in content
+    {
+        let fixedline = line.replace("\r", "");
+        let rps_match: Vec<&str> = fixedline.split(' ').collect();
+        let rival = rps_match[0];
+        let my_choice = rps_match[1];
+        final_points += points(rival, my_choice);
+    }
+    return final_points;
+}
 
 fn p2() -> () {}
 
-fn main() {}
+fn main()
+{
+    let content = read_file_filter("input.txt".to_string());
+    let p1_result = p1(&content);
+    println!("P1: {}", p1_result);
+}
