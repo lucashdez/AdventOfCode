ff2462814712c6411a54bcfa69201d9875b070e5
lucashdez <lukastpiano@gmail.com>
2023-11-08 01:46:52 +0000

    NOne yet

--- 2015/Problema18/main.ts
+++ 2015/Problema18/main.ts
@@ -1,7 +1,8 @@
-async function draw(arr: boolean[][]) {
+const size = 6
+async function draw(readonly arr: boolean[][]) {
 	let auxstr = "";
-	for (let i = 0; i < arr.length; i++) {
-		for (let j = 0; j < arr[i].length; j++) {
+	for (let i = 0; i < size; i++) {
+		for (let j = 0; j < size; j++) {
 			auxstr += arr[i][j]? "#" : "."; 
 		}
 		auxstr += "\n";
@@ -12,19 +13,44 @@ async function draw(arr: boolean[][]) {
 	console.log(auxstr);
 }
 
-const switchOn()
+
+function neighboors(readonly grid: boolean[][], row: number, col: number) {
+	let num = 0;
+	if( row>0 ) {
+		if( col > 0 && grid[row-1][col-1]) num += 1;
+		if( grid[row-1][col]) num += 1;
+		if( col < size-1 && grid[row-1][col+1]) num += 1;
+	}
+    if( col > 0 && grid[row][col-1]) num += 1;
+    if( col < size-1 && grid[row][col+1]) num += 1;
+	if( row< size-1 ) {
+		if( col > 0 && grid[row+1][col-1]) num += 1;
+		if( grid[row+1][col]) num += 1;
+		if( col < size-1 && grid[row+1][col+1]) num += 1;
+	}
+	
+	return num;
+}
 
 function P1(arr: boolean[][]) {
 	let iter = 4;
 	let old_one = arr;
 	let new_one = arr;
 
-	for (let i = 0; i < iter; ++i) {
-		for (let j = 0; j < arr[i].length; ++j) {
-
+	for (let iterator = 0; iterator < iter; iterator++) {
+		for(let i = 0; i < size; i++) {
+			for (let j = 0; j < size; j++) {
+				let nn = neighboors(old_one, i, j);
+				if (old_one[i][j]) {
+					new_one[i][j] = (nn==2 || nn == 3);
+				} else {
+					new_one[i][j] = (nn == 3);
+				}
+			}
 		}
+		old_one = new_one;
+		draw(new_one);
 	}
-
 }
 
 function P2() {
@@ -41,9 +67,15 @@ function main() {
 #.#..#
 ####..
 `;
-	const data = readed.split("\n").map((line) => line.split("").map((c) => c === "#"));
-
+	//const data = readed.split("\n").map((line) => line.split("").map((c) => c === "#"));
+	const data = [[false,true,false,true,false,true],
+				  [false,false,false,true,true,false],
+				  [true,false,false,false,false,true],
+				  [false,false,true,false,false,false],
+				  [true,false,true,false,false,true],
+				  [true,true,true,true,false,false]] 
 	draw(data);
+
 	let p1_res = P1(data);
 	let p2_res = P2();
 	console.log(`P1: ${p1_res}\nP2: ${p2_res}`);
