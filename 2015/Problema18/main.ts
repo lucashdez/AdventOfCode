0b1f9bd4d17ff54c6ba7cbafa1352c4e978b43ae
lucashdez <lukastpiano@gmail.com>
2023-11-09 00:12:51 +0000

    Whats happeninggg

--- 2015/Problema18/main.ts
+++ 2015/Problema18/main.ts
@@ -3,54 +3,62 @@ async function draw(readonly arr: boolean[][]) {
 	let auxstr = "";
 	for (let i = 0; i < size; i++) {
 		for (let j = 0; j < size; j++) {
-			auxstr += arr[i][j]? "#" : "."; 
+			auxstr += arr[i][j]? "█" : " "; 
 		}
+		
 		auxstr += "\n";
 	}
-
-	//await new Promise((f) => setTimeout(f, 1000))
-	console.clear();
+	await new Promise(r => setTimeout(r, 100));
+	console.clear()
 	console.log(auxstr);
 }
 
+function valid(i,j) {
+	return i>=0 && i<size && j>=0 && j<size
+}
 
-function neighboors(readonly grid: boolean[][], row: number, col: number) {
+const nei = [{x:-1, y:-1},
+			 {x:-1, y:0},
+			 {x:-1, y:1},
+			 {x:0, y:-1},
+			 {x:0, y:1},
+			 {x:1, y:-1},
+			 {x:1, y:0},
+			 {x:1, y:1}]
+
+function neighboors(readonly grid: boolean[][], i: number, j: number) {
 	let num = 0;
-	if( row>0 ) {
-		if( col > 0 && grid[row-1][col-1]) num += 1;
-		if( grid[row-1][col]) num += 1;
-		if( col < size-1 && grid[row-1][col+1]) num += 1;
-	}
-    if( col > 0 && grid[row][col-1]) num += 1;
-    if( col < size-1 && grid[row][col+1]) num += 1;
-	if( row< size-1 ) {
-		if( col > 0 && grid[row+1][col-1]) num += 1;
-		if( grid[row+1][col]) num += 1;
-		if( col < size-1 && grid[row+1][col+1]) num += 1;
-	}
-	
+	nei.map((d) => {
+		if(valid(i+d.x, j+d.y)) {
+			num += grid[i+d.x][j+d.y]? 1 : 0;
+		}
+	});
 	return num;
 }
 
-function P1(arr: boolean[][]) {
-	let iter = 4;
+async function P1(arr: boolean[][]) {
+	let iter = 100;
 	let old_one = arr;
 	let new_one = arr;
 
 	for (let iterator = 0; iterator < iter; iterator++) {
 		for(let i = 0; i < size; i++) {
-			for (let j = 0; j < size; j++) {
-				let nn = neighboors(old_one, i, j);
+			for(let j = 0; j < size; j++) {
+				const nn = neighboors(old_one, i, j);
 				if (old_one[i][j]) {
-					new_one[i][j] = (nn==2 || nn == 3);
+					new_one[i][j] = (nn != 2 && nn != 3)? false : true;
 				} else {
-					new_one[i][j] = (nn == 3);
+					new_one[i][j] = (nn === 3)? true : false;
 				}
 			}
 		}
 		old_one = new_one;
-		draw(new_one);
+		await draw(old_one)
 	}
+
+	let count = 0;
+	new_one.forEach((row) => row.forEach((col) => count += col ? 1 : 0));
+	return count;
 }
 
 function P2() {
@@ -58,26 +66,12 @@ function P2() {
 
 
 
-function main() {
-	const readed = `
-.#.#.#
-...##.
-#....#
-..#...
-#.#..#
-####..
-`;
-	//const data = readed.split("\n").map((line) => line.split("").map((c) => c === "#"));
-	const data = [[false,true,false,true,false,true],
-				  [false,false,false,true,true,false],
-				  [true,false,false,false,false,true],
-				  [false,false,true,false,false,false],
-				  [true,false,true,false,false,true],
-				  [true,true,true,true,false,false]] 
-	draw(data);
+async function main() {
+	const readed = ".#.#.#\n...##.\n#....#\n..#...\n#.#..#\n####..\n";
+	const data = readed.split("\n").map((line) => line.split("").map((c) => c === "#")).filter(d => d.length !== 0);
 
-	let p1_res = P1(data);
-	let p2_res = P2();
+	let p1_res =  P1(data);
+	let p2_res =  P2();
 	console.log(`P1: ${p1_res}\nP2: ${p2_res}`);
 }
 
