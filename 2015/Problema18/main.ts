e99d7f177c3cf29f21071b6c860a0df10b06b812
Lucas Hernández Abreu <60920384+lucashdez@users.noreply.github.com>
2023-11-09 16:33:15 +0000

    Update main.ts

--- 2015/Problema18/main.ts
+++ 2015/Problema18/main.ts
@@ -1,5 +1,5 @@
 const size = 6
-async function draw(readonly arr: boolean[][]) {
+async function draw(readonly arr: number[][]) {
 	let auxstr = "";
 	for (let i = 0; i < size; i++) {
 		for (let j = 0; j < size; j++) {
@@ -17,60 +17,73 @@ function valid(i,j) {
 	return i>=0 && i<size && j>=0 && j<size
 }
 
-const nei = [{x:-1, y:-1},
-			 {x:-1, y:0},
-			 {x:-1, y:1},
-			 {x:0, y:-1},
-			 {x:0, y:1},
-			 {x:1, y:-1},
-			 {x:1, y:0},
-			 {x:1, y:1}]
-
-function neighboors(readonly grid: boolean[][], i: number, j: number) {
-	let num = 0;
-	nei.map((d) => {
-		if(valid(i+d.x, j+d.y)) {
-			num += grid[i+d.x][j+d.y]? 1 : 0;
-		}
-	});
-	return num;
+function get_data(readonly grid:number[][], i, j) {
+	if(valid(i, j))
+		return grid[i][j]
+	return 0
 }
 
-async function P1(arr: boolean[][]) {
-	let iter = 100;
-	let old_one = arr;
-	let new_one = arr;
+function get_sum(readonly grid:number[][], i, j) {
+	let sum = 0;
 
-	for (let iterator = 0; iterator < iter; iterator++) {
-		for(let i = 0; i < size; i++) {
-			for(let j = 0; j < size; j++) {
-				const nn = neighboors(old_one, i, j);
-				if (old_one[i][j]) {
-					new_one[i][j] = (nn != 2 && nn != 3)? false : true;
-				} else {
-					new_one[i][j] = (nn === 3)? true : false;
-				}
-			}
+	sum += get_data(grid, i - 1, j - 1) ;
+	sum += get_data(grid, i - 1, j) ;
+	sum += get_data(grid, i - 1, j + 1) ;
+	sum += get_data(grid, i, j - 1) ;
+	sum += get_data(grid, i, j + 1) ;
+	sum += get_data(grid, i + 1, j - 1) ;
+	sum += get_data(grid, i + 1, j) ;
+	sum += get_data(grid, i + 1, j + 1) ;
+
+	return sum
+}
+
+function update(original: number[][]) {
+	let newa = original;
+	for(let i = 0; i < size; i++) {
+		for(let j = 0; j < size; j++) {
+			const nn = get_sum(original, i, j);
+			let state: number;
+			if (original[i][j] == 1) 
+				state = (nn == 2 || nn == 3)? 1 : 0;
+			else 
+				state = (nn == 3)? 1 : 0;
+			newa[i][j] = state;
 		}
-		old_one = new_one;
-		await draw(old_one)
 	}
+	original = newa;
+}
 
-	let count = 0;
-	new_one.forEach((row) => row.forEach((col) => count += col ? 1 : 0));
-	return count;
+async function P1(arr: number[][]) {
+	let iter = 4;
+	for (let iterator = 0; iterator < iter; iterator++) {
+		update(arr);
+		await draw(arr);
+	}
+	let result = arr
+	return arr;
 }
 
 function P2() {
 }
 
 
+function count(arr: number[][]) {
+	let count = 0;
+	for (let i = 0; i < size; i++)
+		for (let j = 0; j < size; j++)
+			count += arr[i][j]? 1 : 0;
+	return count;
+}
+
 
 async function main() {
 	const readed = ".#.#.#\n...##.\n#....#\n..#...\n#.#..#\n####..\n";
-	const data = readed.split("\n").map((line) => line.split("").map((c) => c === "#")).filter(d => d.length !== 0);
+	const data = readed.split("\n").map((line) => line.split("").map((c) => (c === "#")? 1 : 0).filter(d => d.length !== 0));
 
-	let p1_res =  P1(data);
+	let ap1 = await P1(data)
+	let p1_res =  count(ap1);
+	
 	let p2_res =  P2();
 	console.log(`P1: ${p1_res}\nP2: ${p2_res}`);
 }
