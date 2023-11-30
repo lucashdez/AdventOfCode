14adbe7f6f654e134c966fcc9ed33f19eac4271d
Lucas Hernández Abreu <lukastpiano@gmail.com>
2023-11-30 16:06:04 +0000

    save this 4 for

--- 2015/Problema21/main.ts
+++ 2015/Problema21/main.ts
@@ -23,18 +23,34 @@
 */
 
 class Item {
-	/**
-	 * Weapon: 0
-	 * Armor: 1
-	 * Rings: 2
-	 */
+
+	public name = "";
+	/** Weapon: 0 Armor: 1 Rings: 2 */
 	public category = -1; 
-	constructor() {
+	public cost = 0;
+	public damage = 0;
+	public armor = 0;
+	constructor(s: string, type: number, c: number, dmg: number, def: number) {
+		this.name = s;
+		this.category = type;
+		this.cost = c;
+		this.damage = dmg;
+		this.armor = def;
 	}
 }
 
 class Combo {
-	constructor(weapon, armor, r1, r2, cost) {
+	public weapon;
+	public armor;
+	public ring1;
+	public ring2;
+	public cost
+	constructor(w: Item, a: Item, r1: Item, r2: Item, c: number) {
+        this.weapon = w;
+		this.armor = a;
+		this.ring1 = r1;
+		this.ring2 = r2;
+		this.cost = w.cost + a.cost + r1.cost + r2.cost;
 	}
 }
 
@@ -43,12 +59,46 @@ class Market {
 	public weapons: Array<Item> = [];
 	public armors: Array<Item> = [];
 	public rings: Array<Item> = [];
+	public generate_combos() {
+		for (let wi = 0; wi < this.weapons.length; ++wi) {
+			for (let ai = 0; ai < this.armors.length; ++ai) {
+				for (let ri1 = 0; ri1 < this.rings.length; ++ri1) {
+					for(let ri2 = ri1; ri2 < this.rings.length; ++ri2) {
+					} 
+				}
+			}
+		}
+	
+	}
+
 	constructor() {
+		this.weapons = [
+			//COLUMNS              t  c   d  a
+			new Item("Dagger",     0, 8,  4, 0),
+			new Item("Shortsword", 0, 10, 5, 0),
+			new Item("Warhammer",  0, 25, 6, 0),
+			new Item("Longsword",  0, 40, 7, 0),
+			new Item("Greataxe",   0, 74, 8, 0)
+		]
+		this.armors = [
+			//COLUMNS              t  c    d  a
+			new Item("Leather",    1, 13,  0, 1),
+			new Item("Chainmail",  1, 31,  0, 2),
+			new Item("Splitmail",  1, 53,  0, 3),
+			new Item("Bandedmail", 1, 75,  0, 4),
+			new Item("Platemail",  1, 102, 0, 5)
+		]
+		this.rings = [
+			//COLUMNS              t   c    d  a
+			new Item("Damage +1",  2,  25,  1,  0),
+			new Item("Damage +2",  2,  50,  2,  0),
+			new Item("Damage +3",  2, 100,  3,  0),
+			new Item("Defense +1", 2,  20,  0,  1),
+			new Item("Defense +1", 2,  40,  0,  2),
+			new Item("Defense +1", 2,  80,  0,  3)
+		]
 	}
 }
-
-
-
 
 function P1(shop: Market) {
 }
@@ -58,6 +108,7 @@ function P2() {
 
 function main() {
 	const shop = new Market;
+	shop.generate_combos();
 	let p1 = P1(shop);
 	let p2 = P2();
 	console.log(`P1: ${p1}\nP2: ${p2}`)
