45cd0655d6c11c376fee93321bb4697d123f90e7
lucashdez <lukastpiano@gmail.com>
2023-12-02 14:17:45 +0000

    Finished 1

--- 2023/problem01/src/main.rs
+++ 2023/problem01/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn p1(input: &[&str]) -> u32 {
 	let mut result: u32 = 0;
 	input.into_iter().for_each(|s| {
@@ -13,6 +15,44 @@ fn p1(input: &[&str]) -> u32 {
 
 fn p2(input: &[&str]) -> u32 {
 	let mut result = 0;
+	let mut numbers_hash: HashMap<&str,&str> = HashMap::new();
+	numbers_hash.insert("one", "1");
+	numbers_hash.insert("two", "2");
+	numbers_hash.insert("three", "3");
+	numbers_hash.insert("four", "4");
+	numbers_hash.insert("five", "5");
+	numbers_hash.insert("six", "6");
+	numbers_hash.insert("seven", "7");
+	numbers_hash.insert("eight", "8");
+	numbers_hash.insert("nine", "9");
+	numbers_hash.insert("1", "1");
+	numbers_hash.insert("2", "2");
+	numbers_hash.insert("3", "3");
+	numbers_hash.insert("4", "4");
+	numbers_hash.insert("5", "5");
+	numbers_hash.insert("6", "6");
+	numbers_hash.insert("7", "7");
+	numbers_hash.insert("8", "8");
+	numbers_hash.insert("9", "9");
+	input.into_iter().for_each(|s| {
+		let mut f:(&str, usize) = ("", 99999);
+		let mut b:(&str, usize) = ("", 0);
+		numbers_hash
+			.clone()
+			.into_iter()
+			.for_each(|(k, _)| {
+				let mut all_idx: Vec<(usize, &str)> = s.match_indices(k).collect::<Vec<(usize, &str)>>();
+				if !all_idx.is_empty() {
+					all_idx.sort_by(|a,b| {a.0.cmp(&b.0)});
+					f = if all_idx[0].0 <= f.1 {(all_idx[0].1, all_idx[0].0)} else {f};
+					b = if all_idx[all_idx.len()-1].0 >= b.1 {(all_idx[all_idx.len()-1].1, all_idx[all_idx.len()-1].0)} else {b};
+				}
+			});
+		if f.0 != "" {
+			let number: u32 = format!("{}{}",numbers_hash.get(f.0).unwrap(),numbers_hash.get(b.0).unwrap()).as_str().parse().unwrap();
+			result += number;
+		}
+	});
 	return result;
 }
 
@@ -21,7 +61,7 @@ fn main() {
 	let input = std::fs::read_to_string(path)
 		.expect(format!("Could not read the input file. Not existent or corrupt in {}", path).as_str());
 	let r1: u32 = p1(&input.split("\n").collect::<Vec<&str>>());
-	let r2: u32 = 0;
+	let r2: u32 = p2(&input.split("\n").collect::<Vec<&str>>());
 
 	print!("P1: {}\nP2: {}", r1, r2);
 }
