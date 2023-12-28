e071542506a473fdbf86765cd384074f3cb3ab75
Lucas Hernandez Abreu <lukastpiano@gmail.com>
2023-12-28 16:31:15 +0000

    Real startion of problem07

--- 2023/problem07/src/main.rs
+++ 2023/problem07/src/main.rs
@@ -1,3 +1,19 @@
+mod lib;
+
+fn p1() -> u64 {
+	todo!()
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+
+#[cfg(test)]
+mod test {
+	use super::*;
+	#[test]
+	fn p1_working() {
+		assert_eq!(p1(), 6440);
+	}
 }
