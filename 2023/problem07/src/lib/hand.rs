5e40da7004f678ef09c2596343c0e80d9f916d24
lucashdez <lukastpiano@gmail.com>
2023-12-29 17:26:02 +0000

    Can read cards??

--- 2023/problem07/src/lib/hand.rs
+++ 2023/problem07/src/lib/hand.rs
@@ -8,6 +8,7 @@
  * High card, where all cards' labels are distinct: 23456
  */
 
+#[derive(PartialEq, Eq)]
 enum HandType {
 	FiveOfAKind,
 	FourOfAKind,
@@ -29,20 +30,68 @@ fn get_card_value(c: char) -> usize {
 	}
 }
 
-
+#[derive(Eq)]
 pub struct Hand {
 	hand: Vec<(char, usize)>,
-	t: HandType
+	t: HandType,
+	value: usize,
 }
 
 impl Hand {
-	pub fn new(s: &str) -> Self {
+	pub fn new(s: &str, value: usize) -> Self {
 		Hand {
 			hand: s.chars().map(|c| {return (c, get_card_value(c))}).collect(),
-			t: HandType::Unkown
+			t: HandType::Unkown,
+			value
 		}
 	}
 
 	pub fn check_hand(&mut self) {
+		let mut final_type: HandType = HandType::Unkown;
+		let mut h: std::collections::HashMap<char, usize> = std::collections::HashMap::new();
+		let cop = self.hand.clone();
+		for card in cop {
+			h.entry(card.0).and_modify(|v| *v += 1).or_insert(1);
+		}
+
+		let mut vec_of_values: Vec<usize> = h.into_values().collect();
+		vec_of_values.sort();
+
+		
+		for v in vec_of_values.into_iter() {
+			match v {
+				5 => { final_type = HandType::FiveOfAKind; break; },
+				4 => { final_type = HandType::FourOfAKind; break; },
+				3 | 2 => { }
+				_ => { final_type = HandType::HighCard },
+			}
+		}
+
+		self.t = final_type;
+	}
+}
+
+impl std::cmp::PartialEq for Hand {
+	fn eq(&self, other: &Hand) -> bool {
+		self.t == other.t
+	}
+}
+
+impl std::cmp::PartialOrd for Hand {
+	fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+		Some(self.cmp(other))
+	}
+}
+
+impl std::cmp::Ord for Hand {
+	fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+		let mut i = 0;
+		loop {
+			if self.hand[i] != other.hand[i] {
+				break;
+			}
+			i += 1;
+		}
+		self.hand[i].1.cmp(&other.hand[i].1)
 	}
 }
--- 2023/problem07/src/main.rs
+++ 2023/problem07/src/main.rs
@@ -1,11 +1,29 @@
 mod lib;
+use lib::hand::*;
 
-fn p1() -> u64 {
-	todo!()
+fn p1(s: &str) -> u64 {
+	let mut hands: Vec<Hand> = Vec::new();
+	s.split('\n')
+		.filter(|f| f != &"")
+		.into_iter()
+		.for_each(|c| {
+			let a: Vec<&str> = c.split(" ").filter(|f| f != &"").collect();
+			if a.len() == 2 {
+				hands.push(Hand::new(a[0], a[1].parse().unwrap()))
+			}
+		});
+	let hands_len = hands.len();
+	for i in 0..hands_len {
+		hands[i].check_hand();
+	}
+	hands.sort();
+	0
 }
 
 fn main() {
-    println!("Hello, world!");
+	let str = std::fs::read_to_string("./src/input.txt")
+		.expect("Cannot read. Bad luck.");
+	p1(str.as_str());
 }
 
 
