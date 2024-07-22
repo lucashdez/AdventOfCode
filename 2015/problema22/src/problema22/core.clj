3c2fa310b3eeee3caec6f1398cc122f961d28131
lucashdez <lukastpiano@gmail.com>
2024-07-22 17:02:57 +0100

    Added

--- 2015/problema22/src/problema22/core.clj
+++ 2015/problema22/src/problema22/core.clj
@@ -9,6 +9,11 @@
                  :mana (- (get state :mana) 53)})
   newstate) 
 
+(defn log [state]
+  (do
+    (println state)
+    (read-line)))
+
 
 (defn drain [state]
   (def newstate {:e (get state :e),
@@ -51,21 +56,22 @@
   newstate) 
 
 (defn apply_effects [state]
-  (def newstate {:e {:poison (rest (get (get state :e) :poison)),
-                     :shield (rest (get (get state :e) :shield)),
-                     :recharge (rest (get (get state :e) :recharge))},
-                 :php (get state :php),
-                 :bhp (- (get state :bhp) (if (= (get (get state :e) :poison) ()) 0 3)),
-                 :bdmg (if (= (get (get state :e) :shield) ()) 8 1),
-                 :mana (+ (get state :mana) (if (= (get (get state :e) :recharge) ()) 0 101))})
+  (def newstate [[(rest (get (get state 0) 0))
+                   (rest (get (get state 0) 1))
+                   (rest (get (get state 0) 2))]
+                 (get state 1)
+                 (- (get state 2) (if (= (get (get state 0) 0) ()) 0 3))
+                 (if (= (get (get state 0) 1) ()) 8 1)
+                 (+ (get state 4) (if (= (get (get state 0) 2) ()) 0 101))
+                 ])
   newstate)
 
 (defn boss_attacks [state]
-  (def newstate {:e (get state :e),
-                 :php (- (get state :php) (get state :bdmg)),
-                 :bhp (get state :bhp),
-                 :bdmg (get state :bdmg),
-                 :mana (get state :mana)})
+  (def newstate [(get state 0)
+                 (- (get state 1) (get state 3))
+                 (get state 2)
+                 (get state 3)
+                 (get state 4)])
   newstate)
 
 (defn permutation [xs]
@@ -74,39 +80,31 @@
     (for [x xs y (permutation (disj xs x))] (cons x y))))
 
 (defn available_spells_at_tick [mana]
-  (if (> mana 229)
-    (permutation (set [recharge poison shield drain magic-missile])) 
-    (if (> mana 173)
-      (permutation (set [poison shield drain magic-missile]))
-      (if (> mana 113)
-        (permutation (set [shield drain magic-missile]))
-        (if (> mana 73)
-          (permutation (set [drain magic-missile]))
-          (if (mana > 53)
+  (if (>= mana 229)
+    (set [recharge poison shield drain magic-missile]))
+    (if (>= mana 173)
+      (set [poison shield drain magic-missile])
+      (if (>= mana 113)
+        (set [shield drain magic-missile])
+        (if (>= mana 73)
+          (set [drain magic-missile])
+          (if (>= mana 53)
             [magic-missile]
-            []))))))
+            [])))))
 
 (defn tick [state myturn spells]
   (do
     (def after_e (apply_effects state))
-    (if (and (> (get after_e :bhp) 0)
-             (> (get after_e :php) 0))
-      (do
-        (def available_spells (available_spells_at_tick (get after_e :mana)))
-        ) ; BOSS AND ME ALIVE
-      (if (<= 0 (get after_e :bhp))
-        (do after_e) ; BOSS DEAD
-        (do 0) ; ME DEADhh
-        ) ; BOSS OR ME DEAD
-      )))
+    (boss_attacks after_e)
+    (boss_attacks after_e)
+    )
+  )
 
-(defn main
+(defn main []
   (do
-    (def effects {:poison (),
-                  :shield (),
-                  :recharge ()}) 
+    (def effects [() () ()]) 
     (def spells [magic-missile drain shield poison recharge])
-   (def game_state {:e effects, :php 50, :bhp 55, :bdmg 8, :mana 500})
+   (def game_state [effects 10 13 8 250])
    (def res (tick game_state true spells))
-    res
-))
+   res
+   ))
