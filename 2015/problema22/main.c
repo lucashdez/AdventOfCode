d23aa823d3ab9fc6c0ef13cb07ee9b88160514ea
lucashdez <lukastpiano@gmail.com>
2024-07-29 16:27:06 +0100

    Finished problem 22

--- 2015/problema22/main.c
+++ 2015/problema22/main.c
@@ -4,6 +4,7 @@
 #define MAX_INT
 
 int best = 9999999;
+b32 hard = true;
 
 typedef struct _state {
     int mana, acc_mana, bhp, php;
@@ -29,7 +30,9 @@ void player_turn(State s);
 void boss_turn(State s) {
     every_turn(&s);
     s.php -= (s.shield_turns > 0)? 1 : 8;
-    player_turn(s);
+    if (s.php > 0) {
+        player_turn(s);
+    }
 }
 
 void magic_missile(State s) {
@@ -79,6 +82,12 @@ struct _spells {
 };
 
 void player_turn(State s) {
+    if (hard) {
+        s.php -= 1;
+        if (s.php <= 0) {
+            return;
+        }
+    }
     every_turn(&s);
     if (s.bhp <= 0) {
         best = Min(best, s.acc_mana);
@@ -87,7 +96,7 @@ void player_turn(State s) {
     for (int i = 0;
         i < 5;
         ++i) {
-        if (s.mana > spells[i].cost && (s.acc_mana + spells[i].cost) < best) {
+        if (s.mana >= spells[i].cost && (s.acc_mana + spells[i].cost) < best) {
             s.mana -= spells[i].cost;
             s.acc_mana += spells[i].cost;
             spells[i].f(s);
@@ -101,8 +110,8 @@ void player_turn(State s) {
 }
 
 int main(int argc, char **argv) {
-    State s = {500, 0, 55, 50, 0, 0, 0};
+    State s = {.acc_mana = 0, .mana = 500, .bhp = 55, .php = 50, .poison_turns = 0, .shield_turns = 0, .recharge_turns = 0};
     player_turn(s);
-    printf("best: %d", s.acc_mana);
+    printf("best: %d", best);
     return 0;
 }
