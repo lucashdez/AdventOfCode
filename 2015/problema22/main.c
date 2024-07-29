8ee197e3071225ec6378e26d1b5c4555cde3a85a
lucashdez <lukastpiano@gmail.com>
2024-07-29 16:09:59 +0100

    a

--- 2015/problema22/main.c
+++ 2015/problema22/main.c
@@ -3,10 +3,106 @@
 #include "base/base_include.h"
 #define MAX_INT
 
-static struct MM_BaseMemory *GLOBAL_BASE_ALLOCATOR;
+int best = 9999999;
 
-static int best_yet;
+typedef struct _state {
+    int mana, acc_mana, bhp, php;
+    char shield_turns, poison_turns, recharge_turns; //Char cause char can only be > 0; 
+} State;
+
+void every_turn(State *s) {
+    if (s->shield_turns > 0) {
+        s->shield_turns -= 1;
+    }
+    if (s->poison_turns > 0) {
+        s->poison_turns -= 1;
+        s->bhp -= 3;
+    }
+    if (s->recharge_turns > 0) {
+        s->recharge_turns -= 1;
+        s->mana += 101;
+    }
+}
+
+void player_turn(State s);
+
+void boss_turn(State s) {
+    every_turn(&s);
+    s.php -= (s.shield_turns > 0)? 1 : 8;
+    player_turn(s);
+}
+
+void magic_missile(State s) {
+    s.bhp -= 4;
+    if (s.bhp <= 0) {
+        best = Min(best, s.acc_mana);
+    } else {
+        boss_turn(s);
+    }
+}
+
+void drain(State s) {
+    s.bhp -= 2;
+    s.php += 2;
+    if (s.bhp <= 0) {
+        best = Min(best, s.acc_mana);
+    } else {
+        boss_turn(s);
+    }
+}
+
+void shield(State s) {
+    s.shield_turns += 6;
+    boss_turn(s);
+}
+
+void poison(State s) {
+    s.poison_turns += 6;
+    boss_turn(s);
+}
+
+void recharge(State s) {
+    s.recharge_turns += 5;
+    boss_turn(s);
+}
+
+
+struct _spells {
+    int cost;
+    void (*f)(State); 
+} spells[] = {
+    {53, magic_missile},
+    {73, drain},
+    {113, shield},
+    {173, poison},
+    {229, recharge},
+};
+
+void player_turn(State s) {
+    every_turn(&s);
+    if (s.bhp <= 0) {
+        best = Min(best, s.acc_mana);
+        return;
+    }
+    for (int i = 0;
+        i < 5;
+        ++i) {
+        if (s.mana > spells[i].cost && (s.acc_mana + spells[i].cost) < best) {
+            s.mana -= spells[i].cost;
+            s.acc_mana += spells[i].cost;
+            spells[i].f(s);
+            s.mana += spells[i].cost;
+            s.acc_mana -= spells[i].cost;
+        } else {
+            return;
+        }
+    }
+
+}
 
 int main(int argc, char **argv) {
-    GLOBAL_BASE_ALLOCATOR = mm_create_malloc_base_memory();
+    State s = {500, 0, 55, 50, 0, 0, 0};
+    player_turn(s);
+    printf("best: %d", s.acc_mana);
+    return 0;
 }
