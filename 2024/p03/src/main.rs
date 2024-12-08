9c410ce9a409f0030a30cdc4050d77739b7a0b74
lucashdez <lukastpiano@gmail.com>
2024-12-08 22:34:59 +0000

    P3

--- 2024/p03/src/main.rs
+++ 2024/p03/src/main.rs
@@ -1,5 +1,5 @@
-use std::ptr::NonNull;
-use std::rc::Rc;
+use std::{ops::Range, rc::Rc, collections::VecDeque};
+use regex::*;
 
 struct StateMachine {
 	first: Rc<State>,
@@ -60,8 +60,60 @@ impl State {
 
 
 fn main() {
-	let read = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
-	let mut sm = StateMachine::new();
-	sm.add_state('c', false, true);
-    println!("Hello, world!");
+	// let read: String = String::from("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))");
+	let read = std::fs::read_to_string("input/input.txt").expect("Couldnt read file");
+	let r = Regex::new(r"mul\((\d+),(\d+)\)").expect("Unexpected error");
+	let mut valid: Vec<(i64, (i64, i64))> = Vec::new();
+	for full in r.find_iter(&read) {
+		let inrgx = Regex::new(r"\((\d+),(\d+)\)").unwrap();
+		let (f, [x, y]) = inrgx.captures(full.as_str()).unwrap().extract();
+		let start = full.range().start;
+		let xp: i64 = x.parse().unwrap();
+		let yp: i64 = y.parse().unwrap();
+		let xl = x.len();
+		let yl = y.len();
+		if xl < 4 && xl > 0
+			&& yl < 4 && yl > 0
+		{
+			valid.push((start as i64, (xp, yp)));
+		}
+	}
+
+	let do_reg = Regex::new(r"do\(\)").unwrap();
+
+	let mut dos: Vec<i64> = do_reg.find_iter(&read).map(|cap| cap.range().start as i64).collect::<Vec<i64>>();
+	let don_reg = Regex::new(r"don't\(\)").unwrap();
+	let mut dons: Vec<i64> = don_reg.find_iter(&read).map(|cap| cap.range().start as i64).collect::<Vec<i64>>();
+
+	dos.sort();
+	dons.sort();
+
+	let mut dos_q = VecDeque::from_iter(dos.iter());
+	dos_q.push_front(&0);
+	let mut dons_q = VecDeque::from_iter(dons.iter());
+	let mut ranges: Vec<(Range<i64>, bool)> = Vec::new();
+	let mut last = 0; 
+	let mut last_mode = true;
+
+
+	while !dons_q.is_empty() && !dos_q.is_empty() {
+		if dons_q.front().is_some() && dos_q.front().is_some() && dons_q.front() < dos_q.front() {
+			ranges.push((last..*dons_q.pop_front().unwrap(), last_mode))
+		}
+	}
+
+
+	let max_i = valid[valid.len()-1].0;
+	if ranges[ranges.len()-1].1 {
+		ranges.push((ranges[ranges.len()-1].0.end..max_i+1, true))
+	} else {
+		ranges.push((ranges[ranges.len()-1].0.end..max_i+1, false))
+	}
+
+
+	println!("{}, {}", dos.len(), dons.len());
+	let result_p1: i64 = valid.iter().map(|x| x.1.0 * x.1.1).sum();
+	let result_p2: i64 = valid.iter().filter(|val| ranges.iter().any(|x| x.1 && x.0.contains(&val.0))).map(|x| x.1.0 * x.1.1).sum();
+
+	println!("Part 1: {result_p1}\nPart 2: {result_p2}");
 }
